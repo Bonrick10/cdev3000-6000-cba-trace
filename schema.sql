919d4c6995e074e912743ab4cc606eff025115ca
Joshua Yeoh <z5591569@ad.unsw.edu.au>
2026-07-13 15:40:54 +0000

    fix(db): Fix broken syntax

--- schema.sql
+++ schema.sql
@@ -40,9 +40,9 @@ CREATE TABLE merchant_tags (
     usual_threshold_upper MONEY CHECK (usual_threshold_upper >= 0.0::MONEY),
     suspicious_threshold_upper MONEY CHECK (suspicious_threshold_upper >= 0.0::MONEY),
 
-    CONSTRAINT usual_lower_gt_suspicious_lower CHECK ( usual_threshold_lower >= suspicious_threshold_lower)
-    CONSTRAINT usual_upper_gt_usual_lower CHECK ( usual_threshold_upper >= usual_threshold_lower)
-    CONSTRAINT suspicious_upper_gt_usual_upper CHECK ( suspicious_threshold_upper >= usual_threshold_upper)
+    CONSTRAINT usual_lower_gt_suspicious_lower CHECK (usual_threshold_lower >= suspicious_threshold_lower),
+    CONSTRAINT usual_upper_gt_usual_lower CHECK (usual_threshold_upper >= usual_threshold_lower),
+    CONSTRAINT suspicious_upper_gt_usual_upper CHECK (suspicious_threshold_upper >= usual_threshold_upper)
 );
 
 CREATE TABLE device_sessions (
