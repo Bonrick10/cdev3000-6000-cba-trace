c455c8a4d6605b56535ac6a11c413ba9ee63dce0
Joshua Yeoh <z5591569@ad.unsw.edu.au>
2026-07-14 11:51:41 +0000

    fix(db): Update transaction mock data to fit new unusual merchant threshold

--- mock_data.sql
+++ mock_data.sql
@@ -7,5 +7,5 @@ INSERT INTO merchant_tags(suspicious_threshold_lower, usual_threshold_lower, usu
 INSERT INTO device_sessions (entity_id, device_id, session_start_time) VALUES (1, 'test1''s device', '2000-01-01 00:00:00');
 INSERT INTO device_sessions (entity_id, device_id, session_start_time) VALUES (2, 'test2''s device', '2000-01-01 00:00:00');
 
-INSERT INTO transactions (sender_bsb, sender_account_number, receiver_bsb, receiver_account_number, amount, transaction_time, sender_latitude, sender_longitude, label, merchant_tags, session_id) VALUES (100000, 1, 100000, 2, 1.0, '2001-01-01 00:00:00', 0, 0, 'legitimate', 1, 1);
-INSERT INTO transactions (sender_bsb, sender_account_number, receiver_bsb, receiver_account_number, amount, transaction_time, sender_latitude, sender_longitude, label, merchant_tags, session_id) VALUES (100000, 2, 100000, 1, 2.0, '2001-01-01 00:00:30', 0, 0, 'legitimate', 1, 2);
+INSERT INTO transactions (sender_bsb, sender_account_number, receiver_bsb, receiver_account_number, amount, transaction_time, sender_latitude, sender_longitude, label, merchant_tags, session_id) VALUES (100000, 1, 100000, 2, 10.0, '2001-01-01 00:00:00', 0, 0, 'legitimate', 1, 1);
+INSERT INTO transactions (sender_bsb, sender_account_number, receiver_bsb, receiver_account_number, amount, transaction_time, sender_latitude, sender_longitude, label, merchant_tags, session_id) VALUES (100000, 2, 100000, 1, 50.0, '2001-01-01 00:00:30', 0, 0, 'legitimate', 1, 2);
