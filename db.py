5ccc41d786fcfa428e73a3d5e309ac0677c8b858
Joshua Yeoh <z5591569@ad.unsw.edu.au>
2026-07-16 03:10:50 +0000

    style(pipeline): Fix linting

--- db.py
+++ db.py
@@ -1,9 +1,9 @@
 """ Database Interface """
 
 import os
+import collections
 import psycopg2
 from dotenv import load_dotenv
-import collections
 
 load_dotenv() # load local environment variables from .env
 
@@ -11,7 +11,7 @@ conn = psycopg2.connect(os.environ["DATABASE_URL"])
 cur = conn.cursor()
 
 def get_transaction(sender_bsb, sender_account_number):
-    # Get list of transaction details for a specified sender id
+    """ Get list of transaction details for a specified sender id """
     cur.execute("""
         SELECT *
         FROM transactions
@@ -20,12 +20,13 @@ def get_transaction(sender_bsb, sender_account_number):
     print(cur.fetchall())
     return cur.fetchall()
 
-def get_transaction_surrounding_info(transaction): 
-    # Gets surrounding info of a transaction e.g. entities, branches, accounts merchant tags, device sessions
+def get_transaction_surrounding_info(transaction):
+    """ Gets adjacent info for an incoming transaction to check against rules """
 
-    # note that no data validation, assumes all the IDs are in the db 
-    # Data validation low priority and not main focus of this project 
-    # Also note that tried to use SQL subqueries instead of sending multiple queries through psycopg2 to minimise latency (but subqueries are still pretty bad)
+    # note that no data validation, assumes all the IDs are in the db
+    # Data validation low priority and not main focus of this project
+    # Also note that tried to use SQL subqueries instead of sending multiple queries through
+    # psycopg2 to minimise latency (but subqueries are still pretty bad)
     cur.execute("""
         SELECT 
             json_build_object(
@@ -80,15 +81,19 @@ def get_transaction_surrounding_info(transaction):
         FROM
             device_sessions
             LEFT JOIN merchant_tags ON merchant_tags.id = %(merchant_tags)s
-            LEFT JOIN transactions ON (transactions.sender_bsb = %(sender_bsb)s AND transactions.sender_account_number = %(sender_account_number)s)
+            LEFT JOIN transactions ON (
+                transactions.sender_bsb = %(sender_bsb)s 
+                AND transactions.sender_account_number = %(sender_account_number)s
+                )
         WHERE 
             device_sessions.id = %(session_id)s
         ORDER BY 
             transactions.transaction_time
         LIMIT 1
-    """, collections.defaultdict(lambda: None, transaction)) # defaultdict convert non entries into None - especially for merchant_tags which may not be provided 
-    
-    return(cur.fetchone()[0])
+    """, collections.defaultdict(lambda: None, transaction))
+    # ^ convert non entries into None - especially for merchant_tags which may not be provided
+
+    return cur.fetchone()[0]
 
 print(get_transaction_surrounding_info({
     "sender_bsb": 100000,
@@ -96,7 +101,6 @@ print(get_transaction_surrounding_info({
     "receiver_bsb": 100000,
     "receiver_account_number": 2,
     "amount": 10,
-    # "transaction_time": "2026-01-01 00:00:00+00", 
     "transaction_time": "2001-01-03 00:00:00+00",
     "sender_latitude": 0.0,
     "sender_longitude": 0.0,
