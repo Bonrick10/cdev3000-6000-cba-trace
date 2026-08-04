b3fdcc4929f69ace2a29ee4bd283b14c413472a8
Bonrick10 <z5421297@ad.unsw.edu.au>
2026-08-05 06:19:08 +1000

    generated sting op complete

--- src/db_init/populate_data.py
+++ src/db_init/populate_data.py
@@ -1,10 +1,12 @@
 """Data Population Module"""
 
 import random
-from datetime import datetime, timezone
+from datetime import datetime, timezone, date, timedelta
 from pathlib import Path
+from geopy.distance import geodesic
 
 import db_init.gen_tools as gen_tools
+from db_init.txn_gen_tools.device_tools import gen_new_device_id
 from utils.db import NeonDB
 
 BASE_DIR = Path(__file__).resolve().parent
@@ -19,6 +21,13 @@ STING_END_TIME_1 = datetime(2024, 6, 21, 23, 59, 59, tzinfo=timezone.utc)  # 202
 STING_START_TIME_2 = datetime(2024, 6, 25, 0, 0, 0, tzinfo=timezone.utc)  # 2023-01-01 00:00:00+00
 STING_END_TIME_2 = datetime(2024, 6, 28, 23, 59, 59, tzinfo=timezone.utc)  # 2023-01-01 00:00:00+00
 
+GIVEN_NAMES = ['John','Jane','Michael','Emily','David','Sarah','James','Olivia',
+    'William','Emma','Benjamin','Ava','Lucas','Sophia','Mason','Isabella',
+    'Ethan','Mia','Alexander','Charlotte']
+SURNAMES = ['Smith','Johnson','Williams','Brown','Jones','Garcia','Miller',
+    'Davis','Rodriguez','Martinez','Hernandez','Lopez','Gonzalez',
+    'Wilson','Anderson']
+    
 def generate_seed_data():
     """Populates the database with synthetic data.
     """
@@ -28,10 +37,181 @@ def generate_seed_data():
     print("Generating data")
     db.execute(db.read_sql_file(SQL_DIR / "synthetic_population.sql"))
 
-def gen_sting_txns():
+def init_sting_txns():
+    # Create 5 new accounts and entities and store them in a sting table
+
+    def random_date():
+        start = date(1950, 1, 1)
+        end = date(2000, 12, 31)
+
+        delta_days = (end - start).days
+        offset = random.randint(0, delta_days)
+        return start + timedelta(days=offset)
+    
     db = NeonDB()
     
+    branches = db.query("SELECT * FROM branches")
+    
+    acc_counter = 501
+    ent_counter = 51
+    for _ in range(5):
+        given_name = random.choice(GIVEN_NAMES)
+        surname = random.choice(SURNAMES)
+        dob = random_date()
 
+        branch = random.choice(branches)
+        bsb = branch["bsb"]
+        funds = round(random.random() * 10000, 2)
+
+        account_number = (acc_counter * 7919) % 900000000 + 100000000
+        entity_id = (ent_counter * 7919) % 90000 + 100000
+        
+        lat = random.uniform(-90, 90)
+        lon = random.uniform(-180, 180)
+
+        lat = round(lat, 6)
+        lon = round(lon, 6)
+
+        acc_counter += 1
+        ent_counter += 1
+
+        entity = {
+            "id": entity_id,
+            "given_name": given_name,
+            "surname": surname,
+            "date_of_birth": dob
+        }
+
+        account = {
+            "bsb": bsb,
+            "account_number": account_number,
+            "funds": funds,
+            "entity_id": entity_id
+        }
+
+        sting = {
+            "bsb": bsb,
+            "account_number": account_number,
+            "latitude": lat,
+            "longitude": lon
+        }
+
+        db.execute("""
+            INSERT INTO entities (
+                id,
+                given_name,
+                surname,
+                date_of_birth
+            ) VALUES (
+                %(id)s,
+                %(given_name)s,
+                %(surname)s,
+                %(date_of_birth)s                                                             
+            );
+            """, entity)
+
+        db.execute("""
+            INSERT INTO accounts (
+                bsb,
+                account_number,
+                funds,
+                entity_id
+            ) VALUES (
+                %(bsb)s,
+                %(account_number)s,
+                %(funds)s,
+                %(entity_id)s
+            );
+            """,
+            account)
+
+        db.execute("""INSERT INTO stings (
+                bsb,
+                account_number,
+                latitude,
+                longitude
+            ) VALUES (
+                %(bsb)s,
+                %(account_number)s,
+                %(latitude)s,
+                %(longitude)s   
+            );
+            """,
+            sting)
+    
+
+def gen_sting_txns():
+    def sting_txn_time():
+        # Pick which range to use
+        start, end = random.choice([
+            (STING_START_TIME_1, STING_END_TIME_1),
+            (STING_START_TIME_2, STING_END_TIME_2)
+        ])
+
+        # Compute total seconds in the chosen range
+        delta_seconds = int((end - start).total_seconds())
+
+        # Pick a random offset
+        offset = random.randint(0, delta_seconds)
+
+        return start + timedelta(seconds=offset)
+    
+    def vary_lat_lon(lat, lon):
+        radius_km = 1
+        # Pick a random bearing (0–360 degrees)
+        bearing = random.uniform(0, 360)
+
+        # Pick a random distance inside the circle (uniform distribution)
+        distance = radius_km * (random.random() ** 0.5)
+
+        # Compute the destination point
+        new_point = geodesic(kilometers=distance).destination((lat, lon), bearing)
+
+        return round(new_point.latitude, 6), round(new_point.longitude, 6)
+
+    db = NeonDB()
+    
+    accounts = db.query("""SELECT *
+        FROM accounts a
+        WHERE a.is_merchant = FALSE
+            AND NOT EXISTS (
+                SELECT 1
+                FROM stings s
+                WHERE s.bsb = a.bsb
+                AND s.account_number = a.account_number
+            );
+        """)
+    stings = db.query("SELECT * FROM stings;")
+
+    for n in range(1000):
+        print(f"Generating {n} sting txn out of 1000")
+        txn_time = sting_txn_time() 
+        seed_account = random.choice(accounts)
+        sting = random.choice(stings)
+
+        amount = round(random.uniform(500, 1000), 2)
+
+        merchant_tag = None
+        lat, lon = vary_lat_lon(sting["latitude"], sting["longitude"])
+        device_id = gen_new_device_id(seed_account["entity_id"], txn_time, db)
+        
+        txn = {
+                "sender_bsb": seed_account["bsb"],
+                "sender_account_number": seed_account["account_number"],
+                "receiver_bsb": sting["bsb"],
+                "receiver_account_number": sting["account_number"],
+                "amount": amount,
+                "transaction_time": txn_time,
+                "sender_latitude": lat,
+                "sender_longitude": lon,
+                "predicted_label": "legitimate",
+                "true_label": "confirmed_fraudulent",
+                "merchant_tags": merchant_tag,
+                "device_id": device_id,
+            }
+        insert_txn(txn, db, "sting_txns")
+
+        
 
 def gen_all_txns():
     db = NeonDB()
@@ -118,7 +298,7 @@ def gen_all_txns():
 #         """, correction)
 
 
-def insert_txn(txn, db):
+def insert_txn(txn, db, table="transactions"):
     """Insert a transaction into the database.
 
     Args:
@@ -126,35 +306,17 @@ def insert_txn(txn, db):
         db: NeonDB instance for database operations.
 
     """
-    db.execute(
-        """
-        INSERT INTO transactions (
-            sender_bsb,
-            sender_account_number,
-            receiver_bsb,
-            receiver_account_number,
-            amount,
-            transaction_time,
-            sender_latitude,
-            sender_longitude,
-            predicted_label,
-            true_label,
-            merchant_tags,
-            device_id
-        ) VALUES (
-            %(sender_bsb)s,
-            %(sender_account_number)s,
-            %(receiver_bsb)s,
-            %(receiver_account_number)s,
-            %(amount)s,
-            %(transaction_time)s,
-            %(sender_latitude)s,
-            %(sender_longitude)s,
-            %(predicted_label)s,
-            %(true_label)s,
-            %(merchant_tags)s,
-            %(device_id)s
-        );
-        """,
-        txn,
+    sql = f"""
+    INSERT INTO {table} (
+        sender_bsb, sender_account_number, receiver_bsb, receiver_account_number,
+        amount, transaction_time, sender_latitude, sender_longitude, predicted_label,
+        true_label, merchant_tags, device_id
     )
+    VALUES (
+        %(sender_bsb)s, %(sender_account_number)s, %(receiver_bsb)s, %(receiver_account_number)s,
+        %(amount)s, %(transaction_time)s, %(sender_latitude)s, %(sender_longitude)s, %(predicted_label)s,
+        %(true_label)s, %(merchant_tags)s, %(device_id)s
+    )
+    """
+
+    db.execute(sql, txn)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 """Main Module"""
 
-from db_init.populate_data import gen_all_txns, generate_seed_data
+from db_init.populate_data import gen_all_txns, generate_seed_data, init_sting_txns, gen_sting_txns
 
 if __name__ == "__main__":
     # generate_seed_data()
-    gen_all_txns()
+    # gen_all_txns()
+    # init_sting_txns()
+    gen_sting_txns()
